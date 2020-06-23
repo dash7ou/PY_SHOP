d0b75b6df4b488852e5f3dd37ad2b037042d5907
DasH <fxfddfno@gmail.com>
2020-06-23 17:38:59 +0300

    show tempelets

--- products/views.py
+++ products/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Product
 
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("Hello from django")
+    products = Product.objects.all()
+    return render(request, 'index.html')
 
 
 def new(request):
