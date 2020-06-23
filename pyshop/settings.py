db7c4b88720c069445ce1bcf061c41091b3ef075
DasH <fxfddfno@gmail.com>
2020-06-23 16:59:52 +0300

    connect to mongodb database and create magrations then run it

--- pyshop/settings.py
+++ pyshop/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'products.apps.ProductsConfig'
+
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,11 @@ WSGI_APPLICATION = 'pyshop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'djongo',
+        'ENFORCE_SCHEMA': True,
+        'NAME': 'pyshop_db',
+        'HOST': 'mongodb://localhost',
+        'PORT': "27017"
     }
 }
 
